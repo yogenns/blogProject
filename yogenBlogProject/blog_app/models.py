94509b93c4ab3b945438b9133ca415aa69e8cfde
yogenns <yogenns@gmail.com>
2020-08-10 14:29:59 +0530

    Create Views and Models

--- yogenBlogProject/blog_app/models.py
+++ yogenBlogProject/blog_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self:
+        return self.comments.filter(approved_comment=True)
+
+    #This is a method django looks for when post is saved and django needs to redirect
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey('blog_app.post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approve_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+        
+    def __str__(self):
+        return self.text
+
+
--- yogenBlogProject/blog_app/urls.py
+++ yogenBlogProject/blog_app/urls.py
@@ -2,5 +2,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('',views.IndexView.as_view(),name='index'),
+    #url('',views.IndexView.as_view(),name='index'),
+    url('',views.PostListView.as_view(),name='post_list'),
+    url('about/',views.AboutView.as_view(),name='about'),
+    url('post/(?P<pk>\d+)',views.PostDetailsView.as_view(),name='post_detail'),
+    url('post/new/',views.CreatePostView.as_view(),name='post_new'),
+    url('post/edit/(?P<pk>\d+)',views.UpdatePostView.as_view(),name='post_edit'),
+    url('post/delete/(?P<pk>\d+)',views.PostDeleteView.as_view(),name='post_delete'),
+    url('drafts/',views.DraftListView.as_view(),name='draft_list'),
+    url('post/(?P<pk>\d+/comment/)',views.add_comment_to_post,name='add_comment_to_post'),
+    url('comment/(?P<pk>\d+/approve/)',views.comment_approve,name='comment_approve'),
+    url('comment/(?P<pk>\d+/remove/)',views.comment_remove,name='comment_remove'),
+    url('post/(?P<pk>\d+/publish/)',views.post_publish,name='post_publish'),
 ]
